3a8cdae36a49b156594b005a709935b209127884
CodeInferno03 <ishaansingh2610@gmail.com>
2024-05-06 18:27:39 -0700

    Hopefully working /login route

--- CodeInferno-Web-App/src/main.jsx
+++ CodeInferno-Web-App/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import LoginPage from './pages/LoginPage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div />,
+    errorElement: <div />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <div><p>Error rendering LoginPage.</p></div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}  />
   </React.StrictMode>,
 )
---
+++ CodeInferno-Web-App/src/pages/LoginPage.jsx
@@ -0,0 +1,34 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom/client"
+
+// import {
+//     createBrowserRouter,
+//     RouterProvider,
+// } from "react-router-dom"
+
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <div>Hello World</div>
+//     }
+// ])
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <RouterProvider router={router} />
+//     </React.StrictMode>
+// )
+
+import LoginBox from "../components/loginPageBox"
+
+function LoginPage() {
+
+    return (
+        <>
+          <LoginBox />
+        </>
+    );
+}
+
+export default LoginPage;
