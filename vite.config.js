7ff1e74faaff93dc4eaba07bcfc85cd865bf1271
CodeInferno03 <ishaansingh2610@gmail.com>
2024-07-04 00:14:23 -0700

    massively changed folder structure

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CodeInferno03.github.io/', // Ensure the base path is correct
-  build: {
-    outDir: '../dist' // adjust the output directory as needed
-  }
+  // base: '/CodeInferno03.github.io/', // Ensure the base path is correct
+  // build: {
+  //   outDir: '../dist' // adjust the output directory as needed
+  // }
 });
